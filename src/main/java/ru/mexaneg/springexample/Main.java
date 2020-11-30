14a66908d72764da0feede9c4852f16ab56d6425
Alexander Yarovitsky <alexander.yarovitsky@tibbo.com>
2020-11-30 18:37:43 +0300

    [UPD] Implement DC (creating musicPlayer)

--- src/main/java/ru/mexaneg/springexample/Main.java
+++ src/main/java/ru/mexaneg/springexample/Main.java
@@ -5,9 +5,10 @@ import org.springframework.context.support.*;
 public class Main {
     public static void main(String[] args){
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = applicationContext.getBean("musicBean", Music.class);
+        //Music music = applicationContext.getBean("musicBean", Music.class);
 
-        MusicPlayer player = new MusicPlayer(music);
+        //MusicPlayer player = new MusicPlayer(music);
+        MusicPlayer player = applicationContext.getBean(MusicPlayer.class, "musicPlayer");
         player.playMusic();
 
         applicationContext.close();

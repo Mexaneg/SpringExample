4efd7f44a7038a2cb90d918602ea8215f4c207a4
Alexander Yarovitsky <alexander.yarovitsky@tibbo.com>
2020-12-01 18:17:03 +0300

    [UPD] Use prototype scope

--- src/main/java/ru/mexaneg/springexample/Main.java
+++ src/main/java/ru/mexaneg/springexample/Main.java
@@ -5,8 +5,14 @@ import org.springframework.context.support.*;
 public class Main {
     public static void main(String[] args){
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer player = applicationContext.getBean(MusicPlayer.class, "musicPlayer");
-        player.playMusic();
+        MusicPlayer firstPlayer = applicationContext.getBean("musicPlayer",MusicPlayer.class);
+        MusicPlayer secondPlayer = applicationContext.getBean("musicPlayer",MusicPlayer.class);
+        secondPlayer.setName("Second Player");
+        firstPlayer.setVolume(10);
+        firstPlayer.playMusic();
+        secondPlayer.playMusic();
+
+        System.out.println("compare players: "+(firstPlayer==secondPlayer));
 
         applicationContext.close();
     }

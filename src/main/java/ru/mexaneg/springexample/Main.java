8a967068a05e6dee4e077a07fc896a797e75c090
Alexander Yarovitsky <alexander.yarovitsky@tibbo.com>
2020-12-01 16:45:43 +0300

    [UPD] cleanup code

--- src/main/java/ru/mexaneg/springexample/Main.java
+++ src/main/java/ru/mexaneg/springexample/Main.java
@@ -5,9 +5,6 @@ import org.springframework.context.support.*;
 public class Main {
     public static void main(String[] args){
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        //Music music = applicationContext.getBean("musicBean", Music.class);
-
-        //MusicPlayer player = new MusicPlayer(music);
         MusicPlayer player = applicationContext.getBean(MusicPlayer.class, "musicPlayer");
         player.playMusic();
 
